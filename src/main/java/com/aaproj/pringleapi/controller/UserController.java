d8a11d3b1a70739e9dae547ba75633335b03e297
aurelianalexandrescuaa <aurelian.alexandrescu@hotmail.com>
2020-11-25 15:12:51 +0200

    Add lombok config

--- src/main/java/com/aaproj/pringleapi/controller/UserController.java
+++ src/main/java/com/aaproj/pringleapi/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.aaproj.pringleapi.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     @GetMapping
     public ResponseEntity<Object> getUsers() {
+        LOGGER.info("/user called Yo");
         return ResponseEntity.ok(Arrays.asList("dude1", "dude2", "duda3"));
     }
 }
