f3ce948be128996292c082d2f24bc72a7828bc82
Aurelian Alexandrescu <aurelian.alexandrescu@hotmail.com>
2020-11-25 13:17:31 +0200

    Initial commit

---
+++ src/main/java/com/aaproj/pringleapi/PringleApiApplication.java
@@ -0,0 +1,13 @@
+package com.aaproj.pringleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PringleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PringleApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aaproj/pringleapi/PringleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaproj.pringleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PringleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
